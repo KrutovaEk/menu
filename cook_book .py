35a79420dbb2678ed61ddab44c9fd004ed1aebef
KrutovaEkaterina <krutova3113@mail.ru>
2023-07-03 22:33:09 +0500

    Zadanie 1, 2

--- cook_book .py
+++ cook_book .py
@@ -25,3 +25,52 @@ with open('new_menu.txt', encoding='utf-8') as f:
             #print(ing)
             cook_book[dish].append(ing)           
 print(cook_book)
+print()
+
+# Задача 2
+def get_shop_list_by_dishes(dishes, person_count):
+    dishes=dishes
+    person_count=person_count
+    list ={}
+    for i in dishes:
+        for j in cook_book.keys():
+            if j==i:
+                products=(cook_book[j])
+                for k in products:
+                    ingredient=k.get('ingredient_name')
+                    # print(ingredient)
+                    if ingredient in list.keys(): 
+                        x=int(k.get('quantity'))*person_count
+                        #print(x)
+                        y=list.get(ingredient)
+                        #print(y)
+                        for i in y:
+                            for key, value in i.items():
+                                if key=='quantity':
+                                    z=value+x
+                                    #print(z)
+                        list[ingredient]=[]
+                        df={}
+                        quant=k.get('quantity')
+                        meas=k.get('measure')
+                        df['measure']=meas
+                        df['quantity']=z
+                        list[ingredient].append(df)        
+
+                        
+                    else:
+                        list[ingredient]=[]
+                        df={}
+                        quant=k.get('quantity')
+                        meas=k.get('measure')
+                        df['measure']=meas
+                        df['quantity']=int(quant)*person_count
+                        list[ingredient].append(df)
+    
+    return list
+    
+    
+#get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)       
+print(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 3))
+
+
